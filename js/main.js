fb184c681f3b54d287cea704b496cbe93facafd2
Art San Diego <asd.artsandiego@gmail.com>
2016-11-17 00:02:25 +0800

    DRYed main.js and changed class names relative to its purpose

--- js/main.js
+++ js/main.js
@@ -2,14 +2,16 @@ $(document).ready(function() {
     // ===============================
     // TYPED
     // ===============================
-    $("#typedString").typed({
+    $('#typedString').typed({
         stringsElement: $('#toTypeString'),
-        typeSpeed: 10,
-        backDelay: 600,
+        typeSpeed: 15,
+        backDelay: 1000,
         showCursor: true,
-        startDelay: 100,
-        callback: function(){
-            $("#scroll-indicator").animate({opacity: 1}, 1000);
+        startDelay: 500,
+        callback: function() {
+            $('#scroll-indicator').animate({
+                opacity: 1
+            }, 1000);
         }
     });
 
@@ -19,18 +21,18 @@ $(document).ready(function() {
     var movementStrength = 35;
     var height = movementStrength / $(window).height();
     var width = movementStrength / $(window).width();
-    $(".top-image").mousemove(function(e) {
+    $('.top-image').mousemove(function(e) {
         var pageX = e.pageX - ($(window).width() / 2);
         var pageY = e.pageY - ($(window).height() / 2);
         var newvalueX = width * pageX * -1 - 25;
         var newvalueY = height * pageY * -1 - 50;
-        $('.top-image').css("background-position", newvalueX + "px     " + newvalueY + "px");
+        $('.top-image').css('background-position', newvalueX + 'px     ' + newvalueY + 'px');
     });
 
     // ===============================
     // FULL PAGE JS
     // ===============================
-    $("#fullpage").fullpage({
+    $('#fullpage').fullpage({
         anchors: ['intro', 'skills'],
         fitToSection: false,
         loopBottom: true,
@@ -38,15 +40,15 @@ $(document).ready(function() {
         // verticalCentered: false,
         normalScrollElements: '#skills-section-content',
         afterLoad: function(anchorLink) {
-            if (anchorLink == "skills") {
-                var $skillGridChildren = $(".skills-grid").children();
-                $(".awesm-header-dark").addClass('animated fadeInLeftBig');
+            if (anchorLink == 'skills') {
+                var $skillGridChildren = $('.skills-grid').children();
+                $('.awesm-header-dark').addClass('animated fadeInLeftBig');
                 $skillGridChildren.addClass('animated bounceIn');
                 //HEADER ON HIDE DIV AFTER 5 SECONDS
                 setTimeout(function() {
                     $('.awesm-header-dark').removeClass('fadeInLeftBig');
                     $('.awesm-header-dark').addClass('fadeOutLeftBig');
-                }, 3000);
+                }, 5000);
 
             }
         },
@@ -64,12 +66,11 @@ $(document).ready(function() {
     });
     wow.init();
 
-
     // ===============================
     // FULL PAGE NAVIGATION
     // ===============================
-    $("#toggle").click(function() {
-        $(this).toggleClass("active");
+    $('#toggle').click(function() {
+        $(this).toggleClass('active');
         $('#overlay').toggleClass('open');
     });
 
@@ -89,80 +90,86 @@ $(document).ready(function() {
     // SKILLS FILL
     // ===============================
     var percent_number_step = $.animateNumber.numberStepFactories.append(' %');
-    var skillHover = function(elem, fill) {
-        var lvl = 100 - fill;
+    // =WEB========================
+    var webSkills = function(elem, fill) {
+        var levelValue = 100 - fill;
+        //CLICK
         $(elem).click(function(event) {
             //LEVEL
             $(this).children('.level').animate({
-                top: fill+"%",
-            }, 1500);
+                top: fill + '%',
+            }, 1000);
             $(this).find('.level > .level-value').animateNumber({
-                number: lvl+"%",
+                number: levelValue + '%',
                 numberStep: percent_number_step
-            }, 1500);
-        });
-        $(elem).hover(function() {
-            //SHADE
-            $(this).children('.fill').animate({
-                top: '100%'
-            }, 500);
-        }, function() {
-            //SHADE
-            $(this).children('.fill').animate({
-                top: '0%'
-            }, 500);
-        });
-    };
-
-    var interests = function(elem){
-        $(elem).click(function(event) {
+            }, 1000);
+            //DESCRIPTION FOR INTERESTS
             $(this).children('.desc').animate({
                 top: '0%'
             }, 500);
         });
+        //HOVERED
+        $(elem).mouseleave(function(event) {
+            //LEVEL
+            $(this).children('.level').animate({
+                top: '115%'
+            }, 1000);
+            $(this).find('.level > .level-value').animateNumber({
+                number: '0%',
+                numberStep: percent_number_step
+            }, 1000);
+            //DESCRIPTION FOR INTERESTS
+            $(this).children('.desc').animate({
+                top: '-100%'
+            }, 500);
+        });
     };
-
-    skillHover("#jquery-container", 50); //<-- 100 - the value = the % of level value, the value = height of level
-    skillHover("#html-container", 10);
-    skillHover("#sass-container", 50);
-    skillHover("#js-container", 70);
-    skillHover("#android-container", 80);
-    skillHover("#git-container", 40);
-    skillHover("#java-container", 80);
-    skillHover("#angular-container", 90);
-    skillHover("#ai-container", 40);
-    skillHover("#ps-container", 50);
-    skillHover("#php-container", 40);
-    skillHover("#mysql-container", 50);
-    skillHover("#cordova-container");
-    skillHover("#swift-container");
-    skillHover("#laravel-container");
-
-    interests("#cordova-container");
-    interests("#swift-container");
-    interests("#laravel-container");
-
     // =MOBILE========================
-    var mobileSkills = function(elem, val){
+    var mobileSkills = function(elem, val) {
         $(elem).hover(function() {
             $(this).find('.mobile-fill').animate({
-                width: val+"%"
+                width: val + '%'
             }, 1500);
         });
     };
+    //html sass jquery js angular git php mysql android java ai ps
+    var mLvl = [90, 60, 60, 30, 10, 60, 60, 50, 30, 20, 60, 50]; //<- THESE VALUES WILL DIRECTLY BE PUSHED ON MOBILESKILLS
+    var wLvl = []; //<- VALUES HERE WILL BE THE VALUE OF WLVL
+    mLvl.forEach(lvlVal);
+    function lvlVal(element, index, array){
+        var newElement = 100 - element; //VALUES FROM WLVL WILL BE SUBTRACTED TO 100
+        wLvl.push(newElement); //ANSWERS WILL THEN BE PUSHED TO MLVL
+    }
 
-    mobileSkills("#jquery-mobile", 50);
-    mobileSkills("#html-mobile", 90);
-    mobileSkills("#sass-mobile", 50);
-    mobileSkills("#js-mobile", 30);
-    mobileSkills("#android-mobile", 20);
-    mobileSkills("#git-mobile", 60);
-    mobileSkills("#java-mobile", 20);
-    mobileSkills("#angular-mobile", 10);
-    mobileSkills("#ai-mobile", 60);
-    mobileSkills("#ps-mobile", 50);
-    mobileSkills("#php-mobile", 60);
-    mobileSkills("#mysql-mobile", 50);
+    // =WEB===============================
+    webSkills('#html-container',    wLvl[0]);
+    webSkills('#sass-container',    wLvl[1]);
+    webSkills('#jquery-container',  wLvl[2]);
+    webSkills('#js-container',      wLvl[3]);
+    webSkills('#angular-container', wLvl[4]);
+    webSkills('#git-container',     wLvl[5]);
+    webSkills('#php-container',     wLvl[6]);
+    webSkills('#mysql-container',   wLvl[7]);
+    webSkills('#android-container', wLvl[8]);
+    webSkills('#java-container',    wLvl[9]);
+    webSkills('#ai-container',      wLvl[10]);
+    webSkills('#ps-container',      wLvl[11]);
+    webSkills('#cordova-container');
+    webSkills('#laravel-container');
+    webSkills('#swift-container');
+    // =MOBILE===============================
+    mobileSkills('#html-mobile',    mLvl[0]);
+    mobileSkills('#sass-mobile',    mLvl[1]);
+    mobileSkills('#jquery-mobile',  mLvl[2]);
+    mobileSkills('#js-mobile',      mLvl[3]);
+    mobileSkills('#angular-mobile', mLvl[4]);
+    mobileSkills('#git-mobile',     mLvl[5]);
+    mobileSkills('#php-mobile',     mLvl[6]);
+    mobileSkills('#mysql-mobile',   mLvl[7]);
+    mobileSkills('#android-mobile', mLvl[8]);
+    mobileSkills('#java-mobile',    mLvl[9]);
+    mobileSkills('#ai-mobile',      mLvl[10]);
+    mobileSkills('#ps-mobile',      mLvl[11]);
 
     // ===============================
     // CHECK WINDOW SIZE
@@ -171,12 +178,12 @@ $(document).ready(function() {
     var $windowSize = $(window).width();
     var isSizeChanged = function() {
         // var display = ($windowSize < 500) ? "none" : "inherit";
-        if($windowSize < 500){
-            $(".skills-grid").css('display', "none");
-            $(".skills-mobile").css('display', 'inherit');
-        }else{
-            $(".skills-grid").css('display', "inherit");
-            $(".skills-mobile").css('display', 'none');
+        if ($windowSize < 500) {
+            $('.skills-grid').css('display', 'none');
+            $('.skills-mobile').css('display', 'inherit');
+        } else {
+            $('.skills-grid').css('display', 'inherit');
+            $('.skills-mobile').css('display', 'none');
         }
     };
     //CHECK WIDTH AFTER REFRESHING
@@ -187,17 +194,3 @@ $(document).ready(function() {
         isSizeChanged();
     });
 });
-
-// ===============================
-// SCROLL DETECT
-// ===============================
-// var vendorPrefAnim = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
-// $skillCircleChildren.one(vendorPrefAnim, function(){
-//     $skillCircleChildren.removeClass('animated bounceIn');
-// });
-//
-// $(this).scroll(function () {
-//     var $y = $(this).topScroll();
-//     if ($y > 500) {
-//     }
-// });
